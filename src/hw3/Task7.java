177780b691835e22e27f0f2775c4bd0c96eb4943
AleksandraFirsova <63194221+AleksandraFirsova@users.noreply.github.com>
2022-09-24 13:19:34 +0300

    hw3

---
+++ src/hw3/Task7.java
@@ -0,0 +1,16 @@
+package hw3;
+
+public class Task7 {
+    public static void main(String[] args) {
+        short allWeightForWeightLoss = 7000;
+        short weightForWeightLossOneDayFirst = 250;
+        short weightForWeightLossOneDayLast = 500;
+        int amountDayForWeightLossFirst = allWeightForWeightLoss/weightForWeightLossOneDayFirst;
+        int amountDayForWeightLossLast = allWeightForWeightLoss/weightForWeightLossOneDayLast;
+        int averageDays = (amountDayForWeightLossFirst + amountDayForWeightLossLast)/2;
+
+        System.out.println(amountDayForWeightLossFirst + " дней уйдет на похудение, если спортсмен будет терять по " + weightForWeightLossOneDayFirst + " г в день");
+        System.out.println(amountDayForWeightLossLast + " дней уйдет на похудение, если спортсмен будет терять по " + weightForWeightLossOneDayLast + " г в день");
+        System.out.println(averageDays + " дней уйдет в среднем");
+    }
+}
---
+++ src/hw3/Task8.java
@@ -0,0 +1,30 @@
+package hw3;
+
+public class Task8 {
+
+    public static void main(String[] args) {
+        int salaryMashaMonth = 67760;
+        int salaryDenisMonth = 83690;
+        int salaryKristinaMonth = 76230;
+
+        int salaryMashaYear = salaryMashaMonth * 12;
+        int salaryDenisYear = salaryDenisMonth * 12;
+        int salaryKristinaYear = salaryKristinaMonth * 12;
+
+        int newSalaryMashaMonth = ((salaryMashaMonth * 10)/100) + salaryMashaMonth;
+        int newSalaryDenisMonth = ((salaryDenisMonth * 10)/100) + salaryDenisMonth;
+        int newSalaryKristinaMonth = ((salaryKristinaMonth * 10)/100) + salaryKristinaMonth;
+
+        int newSalaryMashaYear = newSalaryMashaMonth * 12;
+        int newSalaryDenisYear = newSalaryDenisMonth * 12;
+        int newSalaryKristinaYear = newSalaryKristinaMonth * 12;
+
+        int difSalaryMasha = newSalaryMashaYear - salaryMashaYear;
+        int difSalaryDenis = newSalaryDenisYear - salaryDenisYear;
+        int difSalaryKristina = newSalaryKristinaYear - salaryKristinaYear;
+
+        System.out.println("Маша теперь получает " + newSalaryMashaMonth + " рублей. Годовой доход вырос на " + difSalaryMasha + " рублей");
+        System.out.println("Денис теперь получает " + newSalaryDenisYear + " рублей. Годовой доход вырос на " + difSalaryDenis + " рублей");
+        System.out.println("Кристина теперь получает " + newSalaryKristinaYear + " рублей. Годовой доход вырос на " + difSalaryKristina + " рублей");
+    }
+}
