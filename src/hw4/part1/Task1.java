70be84db505d65b3e329a84374ed9ef06c0bc9b6
AleksandraFirsova <63194221+AleksandraFirsova@users.noreply.github.com>
2022-09-30 11:10:40 +0300

    hw4

---
+++ src/hw4/part1/Task1.java
@@ -0,0 +1,20 @@
+package hw4.part1;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите возраст:");
+        int age = sc.nextInt();
+        short adulthood = 18;
+        if (age >= adulthood) {
+            System.out.println("Поздравляем!");
+        }
+        if (age < adulthood && age > 0) {
+            System.out.println("Возраст совершеннолетия ещё не наступил, и нужно немного подождать");
+        } else {
+            System.out.println("Возраст введен некорректно");
+        }
+    }
+}
