d737ac3e50343e4e65bf0f60a1177aeb22f56aa9
AleksandraFirsova <63194221+AleksandraFirsova@users.noreply.github.com>
2022-10-20 18:55:06 +0300

    hw10

---
+++ src/hw10/Task3.java
@@ -0,0 +1,9 @@
+package hw10;
+
+public class Task3 {
+    public static void main(String[] args) {
+        String fullName = "Иванов Семён Семёнович";
+        String replaceName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + replaceName);
+    }
+}
