1e08164263d7118cc8c28988b1a75a8f942e2a43
AleksandraFirsova <63194221+AleksandraFirsova@users.noreply.github.com>
2022-10-23 11:39:26 +0300

    hw11

---
+++ src/hw11/Task1.java
@@ -0,0 +1,24 @@
+package hw11;
+
+public class Task1 {
+    public static boolean isLeapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void checkLeapYear(int year) {
+        if (isLeapYear(year) == true) {
+            System.out.println(year + " — високосный год");
+        } else {
+            System.out.println(year + " — не високосный год");
+        }
+    }
+
+    public static void main(String[] args) {
+        int year = 2017;
+        checkLeapYear(year);
+    }
+}
